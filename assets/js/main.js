274b62975beeb068ee8a0f642887cbe0bba4366b
CipiVlad <supercoder.cipi@gmail.com>
2022-05-14 02:48:40 +0200

    done-js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,3 @@
-// console.log("text");
-
-
-
-
 // ################## variablen ########################
 
 const mwstaufschlagen = document.getElementById('mwstaufschlagen');
@@ -11,7 +6,8 @@ const mwstabziehen = document.getElementById('mwstabziehen');
 const nineteen = document.getElementById('nineteen');
 const seven = document.getElementById('seven');
 
-
+let headlineInputBetrag = document.getElementById('headlineInputBetrag');
+let endpreis = document.getElementById('endpreis');
 const inputNettoBetrag = document.getElementById("inputNettoBetrag");
 
 // string to number conversion
@@ -19,6 +15,7 @@ const inputNettoBetrag = document.getElementById("inputNettoBetrag");
 
 let brutto = document.getElementById('brutto');
 let mwstbetrag = document.getElementById('mwstbetrag');
+
 
 
 
@@ -30,23 +27,79 @@ const bruttoMwst = () => {
     // console.log(parseInt((inputNettoBetrag.value)) * (0.19) + parseInt((inputNettoBetrag.value)));
     // console.log(parseInt((inputNettoBetrag.value)) * (0.07) + parseInt((inputNettoBetrag.value)));
 
+    // Condition für : Mehrwertsteur aufschlagen checked, 19% checked
+
     if (nineteen.checked && mwstaufschlagen.checked && inputNettoBetrag.value) {
-        console.log('it works');
-        console.log(parseInt((inputNettoBetrag.value)) * (0.19) + parseInt((inputNettoBetrag.value)));
+        // console.log('it works');
+        // console.log(parseInt((inputNettoBetrag.value)) * (0.19) + parseInt((inputNettoBetrag.value)));
         // console.log(parseInt((inputNettoBetrag.value)) * (0.19) + parseInt((inputNettoBetrag.value)));
 
         mwstbetrag.innerHTML = parseInt((inputNettoBetrag.value)) * (0.19);
+
         // Bruttobetrag ins HTML ausgeben
         brutto.innerHTML = parseInt((inputNettoBetrag.value)) * (0.19) + parseInt((inputNettoBetrag.value));
+    }
+    // Condition für : Mehrwertsteur aufschlagen checked, 7% checked
 
-    } else {
+    else if (seven.checked && mwstaufschlagen.checked && inputNettoBetrag.value) {
+        // console.log('it works');
+        // console.log(parseInt((inputNettoBetrag.value)) * (0.07) + parseInt((inputNettoBetrag.value)));
+        // console.log(parseInt((inputNettoBetrag.value)) * (0.19) + parseInt((inputNettoBetrag.value)));
+
+        mwstbetrag.innerHTML = parseInt((inputNettoBetrag.value)) * (0.07);
+        // Bruttobetrag ins HTML ausgeben
+        brutto.innerHTML = parseInt((inputNettoBetrag.value)) * (0.07) + parseInt((inputNettoBetrag.value));
+
+
+
+        // -----------------------------------------------------------------------------------------------//----
+
+
+
+
+        // Condition für : Mehrwertsteur abziehen checked, 19% checked
+
+    } else if (nineteen.checked && mwstabziehen.checked && inputNettoBetrag.value) {
+        // console.log('it works');
+        // console.log(parseInt((inputNettoBetrag.value)) * (0.19) + parseInt((inputNettoBetrag.value)));
+        // console.log(parseInt((inputNettoBetrag.value)) * (0.19) + parseInt((inputNettoBetrag.value)));
+        mwstbetrag.innerHTML = parseInt((inputNettoBetrag.value)) * (0.16);
+        headlineInputBetrag.innerHTML = `Bruttobetrag (Preis inklusive Mehrwertsteuer) in Euro`;
+
+        // Bruttobetrag ins HTML ausgeben
+        brutto.innerHTML = parseInt((inputNettoBetrag.value)) - parseInt((inputNettoBetrag.value)) * (0.16);
+        endpreis.innerHTML = `Nettobetrag`;
+
+
+        // Condition für : Mehrwertsteur abziehen checked, % checked
+
+
+    } else if (seven.checked && mwstabziehen.checked && inputNettoBetrag.value) {
+        // console.log('it works');
+        // console.log(parseInt((inputNettoBetrag.value)) * (0.07) + parseInt((inputNettoBetrag.value)));
+        // console.log(parseInt((inputNettoBetrag.value)) * (0.19) + parseInt((inputNettoBetrag.value)));
+
+        mwstbetrag.innerHTML = parseInt((inputNettoBetrag.value)) * (0.06);
+        headlineInputBetrag.innerHTML = `Bruttobetrag (Preis inklusive Mehrwertsteuer) in Euro`;
+
+        // Bruttobetrag ins HTML ausgeben
+        brutto.innerHTML = parseInt((inputNettoBetrag.value)) - parseInt((inputNettoBetrag.value)) * (0.06);
+        endpreis.innerHTML = `Nettobetrag`;
+
+    }
+
+
+    // -----------------------------------------------------------------------------------------------//----
+
+
+    // Ausgabe bei fehlender/mangelhafter Auswahl des Users
+
+    else {
         brutto.innerHTML = `Das geht so nicht, Mensch!`;
         console.log("it's not working");
     }
 
-
 }
-// bruttoMwst();
 
 
 
@@ -67,7 +120,12 @@ const bruttoMwst = () => {
 
 // ansonsten:
 // else {
-    // brutto.innerHTML = `Das geht so nicht, Mensch!`;
+// brutto.innerHTML = `Das geht so nicht, Mensch!`;
 // 
 //     console.log("it's not working");
 // }
+
+
+
+
+
